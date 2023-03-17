555bc4b05a58527bd5d1c6f1ecb093135fa99d49
hoang <hoangou01@gmail.com>
2023-03-17 12:32:01 +0700

    17032023

--- tikiproject/tikiapp/admin.py
+++ tikiproject/tikiapp/admin.py
@@ -1,11 +1,55 @@
 from django.contrib import admin
 from .models import Account , Seller , Customer , Category , List_Categoies , Cart_item , Cart , Product , Product_detail , Option , Option_group , Order , Order_item , Payment , Evaluate
 # Register your models here.
-admin.site.register(Category)
-admin.site.register(List_Categoies)
-admin.site.register(Product)
-admin.site.register(Product_detail)
-admin.site.register(Option)
-admin.site.register(Option_group)
-admin.site.register(Seller)
-admin.site.register(Customer)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['id' , 'categoryname' , 'image']
+    search_fields = ['categoryname']
+admin.site.register(Category ,CategoryAdmin)
+class Option_groupAdmin(admin.ModelAdmin):
+    list_display = ['id' , 'name']
+    search_fields = ['name']
+admin.site.register(Option_group , Option_groupAdmin)
+
+class OptionAdmin(admin.ModelAdmin):
+    list_display = ['id' , 'name','option_group']
+    search_fields = ['name']
+admin.site.register(Option , OptionAdmin)
+
+class List_CategoryAdmin(admin.ModelAdmin):
+    list_display = ['id' , 'name' , 'category']
+admin.site.register(List_Categoies , List_CategoryAdmin)
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['id' , 'name' , 'base_price' , 'category_name' , 'categoryDetail' , 'seller']
+    search_fields = ['name' , 'category_name' , 'categoryDetail__name']
+admin.site.register(Product , ProductAdmin)
+
+class ProductDetailAdmin(admin.ModelAdmin):
+    list_display = ['id' , 'name' , 'price' , 'quantity' , 'salable_quantity','active' , 'product' , 'option']
+    search_fields = ['name' , 'price' , 'quantity', 'product__name' , 'option__name']
+admin.site.register(Product_detail , ProductDetailAdmin)
+
+class SellerAdmin(admin.ModelAdmin):
+    list_display = ['id' , 'name' , 'phone' , 'address' , 'isOfficial' , 'isChecked' , 'account']
+    search_fields = ['name' , 'phone' , 'address' ,'account__username' ]
+    list_filter = ['isOfficial' , 'isChecked']
+admin.site.register(Seller , SellerAdmin)
+
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ['id' , 'fullname' , 'address' , 'gender' , 'DOB' , 'account']
+    search_fields = ['fullname' , 'gender' , 'account__username']
+
+admin.site.register(Customer , CustomerAdmin)
+
+class AccountAdmin (admin.ModelAdmin):
+    list_display = ['id' , 'username' , 'role']
+    search_fields = ['username']
+admin.site.register(Account , AccountAdmin)
+
+
+
+
+
+
+
+
--- tikiproject/tikiproject/settings.py
+++ tikiproject/tikiproject/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'tikiapp.apps.TikiappConfig'
+    'tikiapp.apps.TikiappConfig',
+    'ckeditor',
+    'ckeditor_uploader'
 ]
 AUTH_USER_MODEL = 'tikiapp.Account'
 MIDDLEWARE = [
@@ -120,7 +122,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 MEDIA_ROOT = '%s/tikiapp/static/' % BASE_DIR
-
+CKEDITOR_UPLOAD_PATH = "ckeditors/tikiapp/"
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
